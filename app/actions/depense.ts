f7ff24dc6227b19a29ce3f80e162e34b34cf1805
Manel <thestraw7@gmail.com>
2026-06-04 23:51:20 +0200

    ajout next-auth types

--- app/actions/depense.ts
+++ app/actions/depense.ts
@@ -2,8 +2,15 @@
 
 import prisma from '@/lib/prisma'
 import { revalidatePath } from 'next/cache'
+import { getServerSession } from 'next-auth'
 
 export async function creerDepense(formData: FormData) {
+    const session = await getServerSession()
+
+    if (!session?.user?.id) {
+        throw new Error("Tu dois être connecté pour ajouter une dépense.")
+    }
+
     const titre = formData.get('titre') as string
     const prix = parseFloat(formData.get('prix') as string)
     const categorie = formData.get('categorie') as string
@@ -15,14 +22,13 @@ export async function creerDepense(formData: FormData) {
     try {
         await prisma.depense.create({
             data: {
-                titre: titre,
-                prix: prix,
+                titre,
+                prix,
                 category: categorie,
-                userId: "1"
+                userId: session.user.id
             },
         })
 
-        // Revalider toutes les pages qui affichent des dépenses
         revalidatePath('/dashboard')
         revalidatePath('/historique')
 
